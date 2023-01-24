3f188ad0c129e630928389566f67fa59082242eb
jejemerong <9708jjw@gmail.com>
2023-01-24 15:42:25 +0900

    add ColorStyle

---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  static const Color babyPink = Color.fromARGB(255, 255, 217, 217);
+  static const Color babyPurple = Color.fromARGB(156, 210, 162, 255);
+}
