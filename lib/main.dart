0a91601a4d478ad14400bdf9b72a0792d37d1914
jejemerong <9708jjw@gmail.com>
2023-01-24 15:44:12 +0900

    add MadalArt for homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,159 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'utils/colors.dart';
 
 void main() {
   runApp(App());
 }
 
-// TODO: 챌린지 컨테이너 재사용하기!!!
-class ChallengeContainer {
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text('운동'),
-        SizedBox(
-          width: 70,
-        ),
-        Text(
-          '스쿼트',
-        )
-      ],
-    );
-  }
-}
-
-// class CheckBox {
-//   Widget build(BuildContext context) {
-//     return Container(
-//       height: 20,
-//       width: 20,
-//       decoration: BoxDecoration(
-//           color: Colors.black, borderRadius: BorderRadius.circular(4)),
-//     );
-//   }
-// }
-
 // starting point
 class App extends StatelessWidget {
-  final now = new DateTime.now();
+  DateTime now = new DateTime.now();
   String formattedDate = DateFormat('yy.MM.dd').format(DateTime.now());
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color.fromARGB(255, 255, 217, 217),
-          body: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 40,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text('$formattedDate',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.w500,
-                            )),
-                      ],
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 80,
-                ),
-                Column(
-                  children: [
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Text('운동'),
-                            SizedBox(
-                              width: 70,
-                            ),
-                            Text('스쿼트'),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            // TODO: 재사용 위젯으로 만들기!
-                            // Checkbox(),
-                            Container(
-                              height: 20,
-                              width: 20,
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(4)),
-                            )
-                          ],
+        backgroundColor: ColorStyles.babyPink,
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 40,
+            vertical: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text('$formattedDate',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.w500,
+                          )),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
                         ),
-                        Row(
-                          children: [
-                            Text('책 읽기'),
-                            SizedBox(
-                              width: 70,
-                            ),
-                            Text('트랜서핑'),
-                          ],
+                        // alignment: Alignment.center,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
                         ),
-                        Row(
-                          children: [
-                            Text('일기 쓰기'),
-                            SizedBox(
-                              width: 50,
-                            ),
-                            Text('소소노트 완료!'),
-                          ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
                         ),
-                        Row(
-                          children: [
-                            Text('명상'),
-                            SizedBox(
-                              width: 70,
-                            ),
-                            Text('호흡법 5'),
-                          ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
                         ),
-                        Row(
-                          children: [
-                            Text('영어'),
-                            SizedBox(
-                              width: 70,
-                            ),
-                            Text('redkiwi 20'),
-                          ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
                         ),
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ),
-          )),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: ColorStyles.babyPurple,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
