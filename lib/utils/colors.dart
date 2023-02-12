31488757e06385b4469d9a4eae5a31038da997ab
jejemerong <9708jjw@gmail.com>
2023-02-12 21:45:17 +0900

    add main color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ColorStyles {
+  static const Color snowWhite = Color.fromARGB(255, 255, 235, 235);
+  static const Color white = Color.fromARGB(255, 255, 255, 255);
+  static const Color black = Color.fromARGB(255, 0, 0, 0);
   static const Color babyPink = Color.fromARGB(255, 255, 217, 217);
   static const Color babyPurple = Color.fromARGB(156, 210, 162, 255);
 }
