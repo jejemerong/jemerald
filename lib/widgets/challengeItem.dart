14ed9ab6b3ac210bba50275dc84aeba17f326b95
jejemerong <9708jjw@gmail.com>
2023-04-04 00:01:38 +0900

    [Add] challengeItem image 추가

--- lib/widgets/challengeItem.dart
+++ lib/widgets/challengeItem.dart
@@ -2,20 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:jemerald/utils/colors.dart';
 
 class ChallengeItem extends StatefulWidget {
-  final String challenges;
-  ChallengeItem({required this.challenges});
+  final String challengeName;
+  final bool isFinished;
+  ChallengeItem({required this.challengeName, required this.isFinished});
   @override
   _ChallengeItemState createState() => _ChallengeItemState();
 }
 
 class _ChallengeItemState extends State<ChallengeItem> {
+  final isFinished = 'before';
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
       Container(
         width: 250,
         height: 90,
-        child: Center(child: Text(widget.challenges)),
+        // child: Center(child: Text(widget.challenges)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(widget.challengeName),
+            Image(
+              image: AssetImage(
+                  'assets/images/${isFinished}${widget.challengeName}.png'),
+              width: 80,
+              height: 80,
+            )
+          ],
+        ),
         decoration: BoxDecoration(
           color: ColorStyles.snowWhite,
           borderRadius: BorderRadius.all(Radius.circular(
